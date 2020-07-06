a1e172f8e12137a0c900218864d0f44e9ada4ada
timotheemcdonald <timotheemcdonald@gmail.com>
2020-07-06 15:51:47 -0400

    adding my files for the day

--- js/index.js
+++ js/index.js
@@ -40,3 +40,73 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const navBar = document.querySelector('nav');
+const navLinks = navBar.querySelectorAll('a');
+navLinks[0].textContent= 'Services';
+navLinks[1].textContent= 'Product';
+navLinks[2].textContent = 'Vision';
+navLinks[3].textContent = 'Features';
+navLinks[4].textContent = 'About';
+navLinks[5].textContent = 'Contact';
+
+// navBar.style.linkColor = 'green';
+// navBar.setAttribute('color','green');
+
+navLinks[0].style.color = 'green';
+navLinks[1].style.color = 'green';
+navLinks[2].style.color = 'green';
+navLinks[3].style.color = 'green';
+navLinks[4].style.color = 'green';
+navLinks[5].style.color = 'green';
+
+// navBar.appendChild('Confusing');
+// navBar.prependChild('Colors');
+
+const newLinkOne = document.createElement('a');
+newLinkOne.textContent = 'Confusing';
+navBar.appendChild(newLinkOne);
+
+const newLinkTwo = document.createElement('a');
+newLinkTwo.textContent = 'Colors';
+navBar.prepend(newLinkTwo);
+
+newLinkTwo.style.color = 'green';
+newLinkOne.style.color = 'green';
+
+
+
+let headerOne = document.querySelector('h1');
+headerOne.textContent = 'DOM is Awesome!';
+
+let buttonOne = document.querySelector('button');
+buttonOne.textContent = 'Get Started';
+
+let headerFour = document.querySelectorAll('h4');
+headerFour[0].textContent = 'Features';
+headerFour[1].textContent = 'About';
+headerFour[2].textContent = 'Services';
+headerFour[3].textContent = 'Product';
+headerFour[4].textContent = 'Vision';
+headerFour[5].textContent = 'Contact';
+
+ const ctaImg = document.getElementById('cta-img')
+ console.log(ctaImg);
+//  ctaImg.src  = 'img/header-img.png';
+ctaImg.setAttribute('src','img/header-img.png');
+
+const middleImg = document.getElementById('middle-img');
+middleImg.setAttribute('src', 'img/mid-page-accent.jpg');
+
+const midText = document.querySelectorAll('p');
+midText[0].textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+midText[1].textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+midText[2].textContent = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+midText[3].textContent = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+midText[4].textContent = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+midText[5].textContent = '123 Way 456 Street Somewhere, USA';
+midText[6].textContent = '1 (888) 888-8888';
+midText[7].textContent = 'sales@greatidea.io';
+
+const footerText = document.querySelector('footer');
+footerText.textContent = 'Copyright Great Idea! 2018';
